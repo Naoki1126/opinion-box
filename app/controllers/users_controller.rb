79c7e5ac3599f8d44d12bb9693792c37a5ce17c8
Naoki1126 <m.naoki85@gmail.com>
2020-08-13 09:38:47 +0000

    [add]users_controller,[update]config/routes.rb

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+    
+    def new
+    end
+
+    def login
+    end
+
+    def login_form
+    end
+
+    def index
+    end
+
+    def edit
+    end
+
+    def update
+    end
+    
+    def create
+    end
+
+    def destroy
+    end
+
+end
