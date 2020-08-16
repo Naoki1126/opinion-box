6845d5c967191b33a2b2fd3f3a2a530efe61a4ae
Naoki1126 <m.naoki85@gmail.com>
2020-08-16 08:13:47 +0000

    [Update]application.rb 日時表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
     before_action :set_current_user
+    before_action :newtag
+
     def set_current_user
         @current_user = User.find_by(id: session[:user_id])
+    end
+
+    def newtag
+        @tagnew = Tag.new
     end
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,20 +3,19 @@ class TagsController < ApplicationController
     
     def index
         @tags = Tag.all
-        @tagnew = Tag.new
     end
     
     def show
         @tag = Tag.find(params[:id])
         @posts = @tag.posts_tag
         @postnew = Post.new
+
     end
 
     def create
         @tagnew = Tag.new(tag_params)
         @tagnew.save
         flash[:notice] = "タグが作成されました"
-        binding.pry
         redirect_to tag_path(@tagnew) 
     end
 
