1bd331f8513a74c39b415536028f14f9d657852c
Naoki1126 <m.naoki85@gmail.com>
2020-08-16 10:23:05 +0000

    [update]controller/comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,20 @@
 class CommentsController < ApplicationController
 
     def create
+        @post = Post.find(params[:id])
+        @comment = Comment.new
+        @comment.user_id = @current_user.id
+        @comment.save
+
     end
 
     def destroy
+        @comment = Comment.find_by(id: params[:id],post_id: params[:post_id])
+        @comment.destroy
     end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:)
+        
 end
